0de9954157f0f656ad7cf38a7469316c7dbcda7c
PureFallen <PureFallen@eclipso.de>
2021-07-08 18:30:29 +0200

    fixed spaces and tabs

--- lib/db/main.py
+++ lib/db/main.py
@@ -18,16 +18,16 @@ def init_update():
         pass
 
 def check_color():
-	try:
-		from modules.colors import bcolors
-	except:
-		print(f"DEBUG: modules/colors.py missing")
-		exit()
-		
+    try:
+        from modules.colors import bcolors
+    except:
+        print(f"DEBUG: modules/colors.py missing")
+        exit()
+
 def banner():
-	from modules.colors import bcolors
-	title = Figlet(font="banner3-D")
-	print(bcolors.PURPLE + title.renderText("TFARMER"))
+    from modules.colors import bcolors
+    title = Figlet(font="banner3-D")
+    print(bcolors.PURPLE + title.renderText("TFARMER"))
 
 if __name__ == "__main__":
     init_update()
