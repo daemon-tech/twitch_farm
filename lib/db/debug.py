7bbfe2d112065f4d164709097802da30cb6944bd
le3ch-tech <sam.moes15@gmail.com>
2021-07-08 23:38:03 +0200

    push

--- lib/db/debug.py
+++ lib/db/debug.py
@@ -5,11 +5,9 @@ import subprocess
 import socket
 import pyfiglet
 import sys
-import asyncio
-import logging
+import threading
 
 from pyfiglet import Figlet
-from socket import *
 from socket import gethostbyname
 
 subprocess.call('clear', shell=True)
@@ -45,8 +43,10 @@ class irc:
 		from modules.colors import bcolors
 		
 		try:
+			print("debug: ircsocket")
 			global ircsocket
-			ircsocket = socket(AF_INET, SOCK_STREAM)
+			ircsocket = socket.socket()
+			print("DEBUG: ircsocket connect")
 			ircsocket.connect((server, port))
 			print(bcolors.RED + "DEBUG: Connection success: {}, {}".format(server, port))
 			try:
@@ -132,11 +132,30 @@ if __name__ == "__main__":
 	get = irc(data[0], data[1], data [2], data[3])
 	print("DEBUG: bot.connection(data[0], data[1])")
 	get.connection(data[0], data[1], data [2], data[3])
+	bot = irc(data[0], data[1], data [2], data[3])
 	
 	while True:
-		bot = irc(data[0], data[1], data [2], data[3])
+		time.sleep(2)
+		print(bcolors.RED + "DEBUG: buffer = bot.receive()")
 		buffer = bot.receive()
+		print("DEBUG: resp, buffer = buffer.split('\n', 1)")
 		resp, buffer = buffer.split('\n', 1)
+		print("DEBUG: if resp.startswith('PING'):")
 		if resp.startswith('PING'):
 				bot.send("PONG", "")
 				print("Pong send")
+		print("DEBUG: resplit = resp.strip().split()")
+		resplit = resp.strip().split()
+		if resplit[1] == "PRIVMSG":
+			try:
+				msg = resp.strip().split(":", 2) # split(":", 2)
+				msg_split = msg[2].split(" ")
+				chan = msg[1].split("PRIVMSG")[1].strip()
+				user = msg[1].split("!")[0]
+				print("DEBUG: try")
+			except:
+				log(err_log, "******\n{} ER MSG:\n{}\n\nOF MSG:\n{}\n".format(msg, resp))
+				continue
+			if msg_split[0] == "test":
+				print(msg_split[0])
+					
