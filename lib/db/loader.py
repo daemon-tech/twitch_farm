eeb2aea98c8a9f839de0a58bed92de8ea0e10af7
PureFallen <PureFallen@eclipso.de>
2021-07-09 00:23:31 +0200

    refactored loader.py

--- lib/db/loader.py
+++ lib/db/loader.py
@@ -2,19 +2,19 @@ import json
 import os
 
 config_file = open('lib/db/config/config.json')
-debug = json.load(config_file)
+config = json.load(config_file)
+
 try:
-	if debug['debug']:
+
+	if config['debug']:
 		os.system("python3 lib/db/debug.py")
-	elif not debug['debug']:
+	elif not config['debug']:
 		os.system("python3 lib/db/main.py")
 
-	#catch missing ErrorHandling "KeyInterrupt" in main/debug.py"
-	exit()
 except KeyError:
-	pass
-session = input("Want to start debug session? [y/n]")
-if session == "y" or session == "Y":
-	os.system("python3 lib/db/debug.py")
-else:
-	os.system("python3 lib/db/main.py")
+
+	session = input("Want to start debug session? [y/n]")
+	if session == "y" or session == "Y":
+		os.system("python3 lib/db/debug.py")
+	else:
+		os.system("python3 lib/db/main.py")
