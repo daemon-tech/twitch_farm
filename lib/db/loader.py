94afd4cfacdab50d9b690be5f78ce50a51b16d80
PureFallen <PureFallen@eclipso.de>
2021-07-09 00:18:11 +0200

    added loader bypass

--- lib/db/loader.py
+++ lib/db/loader.py
@@ -1,5 +1,18 @@
+import json
 import os
 
+config_file = open('lib/db/config/config.json')
+debug = json.load(config_file)
+try:
+	if debug['debug']:
+		os.system("python3 lib/db/debug.py")
+	elif not debug['debug']:
+		os.system("python3 lib/db/main.py")
+
+	#catch missing ErrorHandling "KeyInterrupt" in main/debug.py"
+	exit()
+except KeyError:
+	pass
 session = input("Want to start debug session? [y/n]")
 if session == "y" or session == "Y":
 	os.system("python3 lib/db/debug.py")
