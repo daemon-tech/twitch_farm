e64cf71d5c662a44ae9482b90bffc4bb8618c4b9
PureFallen <PureFallen@eclipso.de>
2021-07-08 18:30:04 +0200

    fixed typo "session"

--- lib/db/loader.py
+++ lib/db/loader.py
@@ -1,7 +1,7 @@
 import os
 
 session = input("Want to start debug session? [y/n]")
-if session == "y" or sessions == "Y":
+if session == "y" or session == "Y":
 	os.system("python3 lib/db/debug.py")
 else:
 	os.system("python3 lib/db/main.py")
